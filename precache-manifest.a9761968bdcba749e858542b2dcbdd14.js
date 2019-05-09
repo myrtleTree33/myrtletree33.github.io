728753027d0564db2b085d71375ea147e10216ba
jhtong <me@joeltong.org>
2019-05-09 23:44:55 +0800

    Updates

---
+++ precache-manifest.a9761968bdcba749e858542b2dcbdd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c393f44e2c29059d29e54697cc955962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761240f9a5dcd756f85d",
+    "url": "/static/css/main.0d473313.chunk.css"
+  },
+  {
+    "revision": "f2340b5fa0550bceb1f9",
+    "url": "/static/js/2.95e005f8.chunk.js"
+  },
+  {
+    "revision": "761240f9a5dcd756f85d",
+    "url": "/static/js/main.5bccdab4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
